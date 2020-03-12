7ea798124aeae53ef8e0b88d6b6ee0b762ad9fe1
tutus widiya <tutuswidiyaningtyas@gmail.com>
2020-03-12 11:25:22 +0700

    activity main progressbar

--- app/src/main/java/com/example/superhero/MainActivity.java
+++ app/src/main/java/com/example/superhero/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.superhero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressbar);
+
     }
 }
